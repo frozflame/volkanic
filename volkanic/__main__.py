5dc1a4a61b137cea4b10de56cd14280bd9129fa0
frozflame <frozflame@outlook.com>
2021-08-14 21:34:28 +0800

    remove from __future__ imports

--- volkanic/__main__.py
+++ volkanic/__main__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-from __future__ import unicode_literals, print_function
-
 import volkanic.cmdline
 from volkanic.utils import (
     desktop_open, where, where_site_packages
